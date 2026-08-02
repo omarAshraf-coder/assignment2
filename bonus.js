429cd0408434c204bcb4f18bfb0130126eb06030
omar <omar@gmail.com>
2026-08-02 22:57:15 +0300

    Add bonus file

--- bonus.js
+++ bonus.js
@@ -1,3 +1,36 @@
+//solve the problem kth-missing-positive-number on leetcode
+function findKthPositive(arr, k) {
+    let current = 1;
+    let index = 0;
 
-x = 16;
-console.log(x);
+    while (k > 0) {
+        if (index < arr.length && arr[index] === current) {
+            index++;
+        } else {
+            k--;
+            if (k === 0) {
+                return current;
+            }
+        }
+        current++;
+    }
+}
+
+//copy the code that you have submitted on the wevsite inside bonus.js file
+
+function findKthPositive(arr, k) {
+    let current = 1;
+    let index = 0;
+
+    while (k > 0) {
+        if (index < arr.length && arr[index] === current) {
+            index++;
+        } else {
+            k--;
+            if (k === 0) {
+                return current;
+            }
+        }
+        current++;
+    }
+}
